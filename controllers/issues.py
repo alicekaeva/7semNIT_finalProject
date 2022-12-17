13cec881709b68cd0807b56f8b26ba2fe73249f0
alicekaeva <alikaeva.aa@students.dvfu.ru>
2022-12-17 15:52:44 +1000

    зачатки фильтров

--- controllers/issues.py
+++ controllers/issues.py
@@ -9,6 +9,9 @@ import datetime
 def issues():
     conn = get_db_connection()
     check = 0
+    f1 = 0
+    f2 = 0
+    f3 = 0
 
     if request.values.get('new_issue'):
         new_issue = request.values.get('new_issue')
@@ -21,9 +24,15 @@ def issues():
     elif request.values.get('issue'):
         id = int(request.values.get('issue'))
         session['id'] = id
-
+    elif request.values.get('filter'):
+        if int(request.values.get('filter')) == 1:
+            f1 = 1
+        elif int(request.values.get('filter')) == 2:
+            f2 = 1
+        elif int(request.values.get('filter')) == 3:
+            f3 = 1
     df_issues = get_issues(conn)
-    df_table = show_table(session['id'], conn)
+    df_table = show_table(session['id'], f1, f2, f3,conn)
     deadline = show_deadline(session['id'], conn).loc[0, "deadline"]
 
     if deadline < str(datetime.datetime.now().date()):
--- models/issues_model.py
+++ models/issues_model.py
@@ -21,15 +21,52 @@ def add_issue(name, date, conn):
     return cur.lastrowid
 
 
-def show_table(id, conn):
-    return pandas.read_sql('''
-    SELECT article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, start_date AS Начал, end_date AS Закончил, checked AS Проверено
-    FROM issue_article_work AS iaw
-    LEFT JOIN worker AS w ON iaw.worker_id = w.worker_id
-    LEFT JOIN worker AS r ON iaw.responsible_for_work_id = r.worker_id
-    JOIN issue_article USING (issue_article_id)
-    JOIN article USING (article_id)
-    JOIN issue USING (issue_id)
-    JOIN work USING (work_id)
-    WHERE issue_id = :id
-    ''', conn, params={"id": id})
+def show_table(id, f1, f2, f3, conn):
+    if f1 == 1:
+        return pandas.read_sql(f'''
+        SELECT article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, start_date AS Начал, end_date AS Закончил, checked AS Проверено
+        FROM issue_article_work AS iaw
+        LEFT JOIN worker AS w ON iaw.worker_id = w.worker_id
+        LEFT JOIN worker AS r ON iaw.responsible_for_work_id = r.worker_id
+        JOIN issue_article USING (issue_article_id)
+        JOIN article USING (article_id)
+        JOIN issue USING (issue_id)
+        JOIN work USING (work_id)
+        WHERE issue_id = :id AND w.worker_id IS NULL 
+        ''', conn, params={"id": id})
+    elif f2 == 1:
+        return pandas.read_sql(f'''
+        SELECT article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, start_date AS Начал, end_date AS Закончил, checked AS Проверено
+        FROM issue_article_work AS iaw
+        LEFT JOIN worker AS w ON iaw.worker_id = w.worker_id
+        LEFT JOIN worker AS r ON iaw.responsible_for_work_id = r.worker_id
+        JOIN issue_article USING (issue_article_id)
+        JOIN article USING (article_id)
+        JOIN issue USING (issue_id)
+        JOIN work USING (work_id)
+        WHERE issue_id = :id AND start_date IS NOT NULL and end_date IS NULL
+        ''', conn, params={"id": id})
+    elif f3 == 1:
+        return pandas.read_sql(f'''
+        SELECT article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, start_date AS Начал, end_date AS Закончил, checked AS Проверено
+        FROM issue_article_work AS iaw
+        LEFT JOIN worker AS w ON iaw.worker_id = w.worker_id
+        LEFT JOIN worker AS r ON iaw.responsible_for_work_id = r.worker_id
+        JOIN issue_article USING (issue_article_id)
+        JOIN article USING (article_id)
+        JOIN issue USING (issue_id)
+        JOIN work USING (work_id)
+        WHERE issue_id = :id AND checked = 0 and end_date IS NOT NULL
+        ''', conn, params={"id": id})
+    else:
+        return pandas.read_sql(f'''
+        SELECT article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, start_date AS Начал, end_date AS Закончил, checked AS Проверено
+        FROM issue_article_work AS iaw
+        LEFT JOIN worker AS w ON iaw.worker_id = w.worker_id
+        LEFT JOIN worker AS r ON iaw.responsible_for_work_id = r.worker_id
+        JOIN issue_article USING (issue_article_id)
+        JOIN article USING (article_id)
+        JOIN issue USING (issue_id)
+        JOIN work USING (work_id)
+        WHERE issue_id = :id
+        ''', conn, params={"id": id})
