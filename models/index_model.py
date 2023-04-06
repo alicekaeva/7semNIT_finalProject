77c3ce5cc5475ea4c4930b80004582e87f7ae582
alicekaeva <alikaeva.aa@students.dvfu.ru>
2023-04-06 20:40:40 +1000

    еще причесала карточки и таблицы

--- models/index_model.py
+++ models/index_model.py
@@ -3,7 +3,7 @@ import pandas
 
 def show_cards(conn):
     return pandas.read_sql('''
-    SELECT iaw.issue_article_work_id, iaw.work_id, issue_name AS Выпуск, article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, iaw.start_date AS Начал, iaw.end_date AS Закончил, iaw.checked AS Проверено
+    SELECT iaw.issue_article_work_id, iaw.work_id, issue_name AS Выпуск, article_name AS Статья, work_name AS Работа, w.worker_name AS Работник, r.worker_name AS Ответственный, iaw.start_date AS Начал, iaw.end_date AS Закончил
     FROM issue_article_work AS iaw
     JOIN issue_article USING (issue_article_id)
     JOIN issue USING (issue_id)
